f5e15c774ca881d0289ab24f0445831c778cb3e9
Fedos-new <fedluky@gmail.com>
2020-11-13 10:33:24 +0700

    Updates

---
+++ precache-manifest.bf02d1abce1d8550d53a55c85d76422a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628819447a46be5e77dd419b8adf815f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2fb0dfe1abdd69cfa033",
+    "url": "/portfolio/static/css/main.65c76772.chunk.css"
+  },
+  {
+    "revision": "62c755012df5484654a9",
+    "url": "/portfolio/static/js/2.39dfbea6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.39dfbea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb0dfe1abdd69cfa033",
+    "url": "/portfolio/static/js/main.76401e23.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
