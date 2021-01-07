603253db18c66b63492d5af5905e4e4944dd0aac
Fedos-new <fedluky@gmail.com>
2021-01-07 21:30:50 +0700

    Updates

---
+++ precache-manifest.0c73245d97b9545725e81c3dd9a40051.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfccc0f2be90007c3faa9594e5c09844",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "de154afd885a3113aae4",
+    "url": "/portfolio/static/css/main.c8405235.chunk.css"
+  },
+  {
+    "revision": "117b7f4a2647b1bd86d5",
+    "url": "/portfolio/static/js/2.e72b0e82.chunk.js"
+  },
+  {
+    "revision": "08aee544f36ba213a8865d2a35c6879d",
+    "url": "/portfolio/static/js/2.e72b0e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de154afd885a3113aae4",
+    "url": "/portfolio/static/js/main.a36ce28d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4a3d1bb98336152b9e9ab5157470e33e",
+    "url": "/portfolio/static/media/social.4a3d1bb9.png"
+  },
+  {
+    "revision": "9997e76a8a414be7aa14a9d73e288299",
+    "url": "/portfolio/static/media/todolist.9997e76a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.12cf4beb8f46b93b4cd4718d237a641a.js"
+  "/portfolio/precache-manifest.0c73245d97b9545725e81c3dd9a40051.js"
 );
 
 self.addEventListener('message', (event) => {
