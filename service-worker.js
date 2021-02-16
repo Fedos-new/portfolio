67c6396425cd86810688c96d36e6ab92316ab496
Fedos-new <fedluky@gmail.com>
2021-02-16 22:31:49 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0c73245d97b9545725e81c3dd9a40051.js"
+  "/portfolio/precache-manifest.3fdb0490879c52b6838a80249e2aa391.js"
 );
 
 self.addEventListener('message', (event) => {
