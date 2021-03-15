8e9962887aca4fff7f9e2f52dcfad53c02c3d0f1
Fedos-new <fedluky@gmail.com>
2021-03-15 10:34:21 +0700

    update CV and skills

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -11,7 +11,7 @@ export const Skills = () => {
     const dataSkills = [
         {id:1, title:'JS', svgHref:'#js', description: 'Работаю по новым стандартам от ES6 и выше'},
         {id:2, title:'React', svgHref:'#react', description: 'Создаю SPA приложения. Использую Hooks, Redux-Thunk, React Router, Formik, Axios, Material-UI и т.д.'},
-        {id:3, title:'Redux', svgHref:'#redux', description: 'Управляю состоянием приложения с помощью Redux. Знаком с Redux Toolkit'},
+        {id:3, title:'Redux', svgHref:'#redux', description: 'Управляю состоянием приложения с помощью Redux. Redux-thunk, redux-saga. Знаком с Redux Toolkit.'},
         {id:4, title:'Sass', svgHref:'#sass', description: 'Использую препроцессоры SCSS/SASS'},
         {id:5, title:'HTML/CSS', svgHref:'#htmlCss', description: 'Делаю адаптивную кроссбраузерную вёрстку. Flexbox, Grid, Style Module'},
         {id:6, title:'TypeScript', svgHref:'#ts', description: 'Для уменьшения ошибок при написании кода использую типизацию TS'}
